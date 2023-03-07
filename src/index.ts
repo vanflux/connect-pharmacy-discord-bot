de7b70671f4e3a3b73fcdd5e99b576fd7613ff7f
vanflux <lucasabruzzi@hotmail.com>
2023-03-07 16:31:32 -0300

    feat: initial code commit

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import 'dotenv/config';
+import { Client, GatewayIntentBits, REST, Routes } from "discord.js";
+
+const clientId = process.env.CLIENT_ID;
+const token = process.env.DISCORD_TOKEN;
+
+if (!clientId) throw new Error('CLIENT_ID must be provided!');
+if (!token) throw new Error('DISCORD_TOKEN must be provided!');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(clientId), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(token);
