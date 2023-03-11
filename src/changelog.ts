d5127aa5d98958c6d2ac98f0634a41356d6120f0
vanflux <lucasabruzzi@hotmail.com>
2023-03-11 18:22:18 -0300

    feat: added changelog for user commands

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,10 +1,14 @@
 
 const changeLog: { [version: string]: string[] } = {
   '2.2.0': [
-    'Comando /wa status para ver status da conexão com o whatsapp',
+    'Comando "/wa status" para ver status da conexão com o whatsapp',
   ],
   '2.3.0': [
     'Mostrando changelog na mensagem de update de versão do bot',
+  ],
+  '2.3.1': [
+    'Comando "/voice rank"',
+    'Comando "/voice points"',
   ]
 }
 
